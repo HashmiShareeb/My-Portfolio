305351899f17e368c1ae4d439b1f5ae10663815d
shareeb hashmi <shareeb.hashmi@student.howest.be>
2026-01-07 19:13:40 +0100

    feat:update cv 2026

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -85,7 +85,7 @@ const Intro = () => {
 
       {/* CTA */}
       <div className="flex lg:flex-row selection  items-center justify-center gap-10">
-        <CTA href="/cv_shareeb_2025_1.pdf" target="_blank" download>
+        <CTA href="/shareebcv_2026.pdf" target="_blank" download>
           <Download size={32} />
           <h1 className="text-lg font-bold ml-2">Download Resume</h1>
           <span className="sr-only">Resume</span>
